583cdf394c83f7f71c27158dcb82f3996b0bfd97
trigaux <trigaux@cisco.com>
2023-01-25 09:45:53 -0800

    Fixing tests.

--- test/name.cpp
+++ test/name.cpp
@@ -54,20 +54,20 @@ TEST_CASE("QUICR::Name Basic Test ")
 
   quicr::Name hex42( "0x42" );
   CHECK( val42 == hex42 );
-  
+
   quicr::Name val41( 0x41 );
   quicr::Name val43( 0x43 );
   CHECK( val42 + 1 == val43 );
   CHECK( val42 - 1 == val41 );
 
-  //CHECK( (quicr::Name("0x1234") >> 4) ==  quicr::Name("0x123") );
-  //CHECK( (quicr::Name("0x1234") << 4) ==  quicr::Name("0x1230") );
-  
+  CHECK( (quicr::Name("0x1234") >> 4) ==  quicr::Name("0x123") );
+  CHECK( (quicr::Name("0x1234") << 4) ==  quicr::Name("0x12340") );
+
   CHECK( quicr::Name("0x123") < quicr::Name("0x124") );
   CHECK( quicr::Name("0x123") > quicr::Name("0x122") );
 
   CHECK( quicr::Name("0x123") != quicr::Name("0x122") );
 
-  //CHECK( quicr::Name("0x123456789abcd0123456789abcd0") >> 64 ==  quicr::Name( 0x123456789abcd0 ) );
-  CHECK( quicr::Name("0x123456789abcd0FFFFFFFFFFFFFF") +1 ==  quicr::Name("0x123456789abcd10000000000000000" ) );
+  CHECK( (quicr::Name("0x123456789abcd012") >> 64) ==  quicr::Name( 0x12345678 ) );
+  CHECK( quicr::Name("0x123456789abcd0FFFFFFFFFFFFFF") + 1 ==  quicr::Name("0x123456789abcd10000000000000000" ) );
 }
