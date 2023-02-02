f479670a4048a93dbd2703eb6f8cbe2472fd1c8a
trigaux <trigaux@cisco.com>
2023-02-02 14:23:26 -0800

    Fixing up hexendec to cover some runtime cases, but still be safe.

--- include/quicr/hex_endec.h
+++ include/quicr/hex_endec.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <array>
+#include <bitset>
 #include <cstdint>
 #include <iomanip>
+#include <limits>
 #include <sstream>
 #include <vector>
 
@@ -37,7 +39,8 @@ class HexEndec
 public:
   HexEndec()
   {
-    static_assert(Size == (N + ...), "Total bits cannot exceed specified size");
+    static_assert((Size & (Size - 1)) == 0, "Size must be a power of 2");
+    static_assert(Size == (N + ...), "Total bits must be equal to Size");
   }
 
   /**
@@ -53,6 +56,7 @@ public:
   template<typename... UInt_ts>
   static inline std::string Encode(UInt_ts... values)
   {
+    static_assert((Size & (Size - 1)) == 0, "Size must be a power of 2");
     static_assert(Size == (N + ...), "Total bits cannot exceed specified size");
     static_assert(sizeof...(N) == sizeof...(UInt_ts),
                   "Number of values should match distribution of bits");
@@ -66,22 +70,43 @@ public:
   template<typename... UInt_ts>
   static inline std::string Encode(const std::vector<uint8_t>& distribution, UInt_ts... values)
   {
-    static_assert(is_valid_uint<UInt_ts...>::value,
-                  "Arguments must all be unsigned integers");
+    static_assert((Size & (Size - 1)) == 0, "Size must be a power of 2");
+    static_assert(is_valid_uint<UInt_ts...>::value, "Arguments must all be unsigned integers");
+    assert(distribution.size() == sizeof...(UInt_ts) && "Number of values should match distribution of bits");
+
+    std::vector<uint64_t> vals;
+    (vals.push_back(values), ...);
+
+    return Encode(distribution, vals);
+  }
+  static inline std::string Encode(const std::vector<uint8_t>& distribution, const std::vector<uint64_t>& values)
+  {
+    static_assert((Size & (Size - 1)) == 0, "Size must be a power of 2");
+    assert(distribution.size() == values.size() && "Number of values should match distribution of bits");
 
     std::stringstream ss;
     ss << std::hex << "0x";
 
-    size_t num_args = 0;
-    auto get_hex = [&](uint64_t value, size_t i) {
-      std::stringstream ss;
-      ss << std::hex << std::setw(distribution.at(i) / 4) << std::setfill('0');
-      ss << (value & (distribution.at(i) >= sizeof(value) * 8
-                        ? ~0x0ull
-                        : ~(~0ull << distribution.at(i))));
-      return ss.str();
+    std::bitset<Size> bits;
+    uint8_t remaining_bits = Size;
+    int i = 0;
+    for (const auto& value : values)
+    {
+      uint8_t dist = distribution.at(i++);
+      remaining_bits -= dist;
+      bits |= std::bitset<Size>(value) << remaining_bits;
     };
-    (ss << ... << get_hex(values, num_args++));
+
+    if (Size > sizeof(uint64_t) * 8)
+    {
+      static const std::bitset<Size> bitset_divider(std::numeric_limits<uint64_t>::max());
+      ss << std::setw(Size/8) << std::setfill('0') << (bits >> (Size / 2) & bitset_divider).to_ullong();
+      ss << std::setw(Size/8) << std::setfill('0') << (bits & bitset_divider).to_ullong();
+    }
+    else
+    {
+      ss << std::setw(Size/4) << std::setfill('0') << bits.to_ullong();
+    }
 
     return ss.str();
   }
@@ -100,7 +125,7 @@ public:
   template<typename Uint_t = uint64_t>
   static inline std::array<Uint_t, sizeof...(N)> Decode(const std::string& hex)
   {
-    static_assert(Size == (N + ...), "Total bits cannot exceed specified size");
+    static_assert(Size >= (N + ...), "Total bits cannot exceed specified size");
     static_assert(is_valid_uint<Uint_t>::value, "Type must be unsigned integer");
 
     std::vector<uint8_t> distribution;
@@ -128,14 +153,37 @@ public:
                                std::to_string(Size / 4) + " characters (" +
                                std::to_string(Size / 8) + " bytes)");
 
-    int i = 0;
-    std::vector<Uint_t> result(distribution.size());
-    for (const uint8_t& n : distribution) {
-      size_t midpoint = (n / 4);
-      std::string bytes = clean_hex.substr(0, midpoint);
-      clean_hex.erase(0, midpoint);
-      result[i++] = std::stoull(bytes, nullptr, 16);
+    std::bitset<Size> bits;
+    const uint8_t size_of = sizeof(uint64_t) * 2;
+    if (Size > size_of * 4)
+    {
+      size_t midpoint = clean_hex.length() - size_of;    
+      std::string low_bits = "0x" + clean_hex.substr(midpoint, size_of);
+      clean_hex.erase(midpoint, size_of);
+      std::string hi_bits = "0x" + clean_hex;
+
+      auto hi = std::stoull(hi_bits, nullptr, 16);
+      auto low = std::stoull(low_bits, nullptr, 16);
+
+      bits |= std::bitset<Size>(low) | (std::bitset<Size>(hi) << (Size / 2));
     }
+    else
+    {
+      bits |= std::bitset<Size>(std::stoull(clean_hex, nullptr, 16));
+    }
+
+    std::vector<Uint_t> result(distribution.size());
+    for (int i = distribution.size() - 1; i >= 0; --i)
+    {
+      std::bitset<Size> mask = std::bitset<Size>{}.set();
+      
+      auto dist = distribution.at(i);
+      mask <<= dist;
+      mask.flip();
+
+      result[i] = (bits & mask).to_ullong();
+      bits >>= dist;
+    };
 
     return result;
   }
